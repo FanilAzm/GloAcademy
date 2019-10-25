bad36e7aeb3977d75d8c2f911553968453c1d5f0
FanilAzm <fanil.azmagulov@mail.ru>
2019-10-25 17:27:30 +0500

    Задание №5 по курсу JS

---
+++ lesson05*/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let arr = ['43563', '32424', '23424', '54564', '83458', '23134', '44444'];
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i].charAt(0) == 4){
+        console.log(arr[i]);
+    }
+    if(arr[i].charAt(0) == 2){
+        console.log(arr[i]);
+    }
+}
+
+
+
+let n = 100;
+
+for(let i = 2; i < n; i++){
+    for(let j = 2; j < i; j++){
+        if(i % j == 0) break;
+    }
+    console.log(i);
+}
