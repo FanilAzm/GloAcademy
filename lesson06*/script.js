084199ec7a22e9a50c578761034de647bc5df12a
FanilAzm <fanil.azmagulov@mail.ru>
2019-10-30 14:59:08 +0500

    Исправлены ошибки в коде

--- lesson06*/script.js
+++ lesson06*/script.js
@@ -2,16 +2,12 @@
 
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
     body = document.querySelector('body'),
-    div,
-    weekEnd = week[5];
-    // weekEnd.style.fontStyle = 'italic';
-    // console.log(weekEnd);
+    div;
 
 for(let i = 0; i < week.length; i++){
     div = document.createElement('div');
     body.appendChild(div);
     div.innerHTML += week[i];
-    console.log(typeof(div));
-
+    
     // console.log(div);
 }
