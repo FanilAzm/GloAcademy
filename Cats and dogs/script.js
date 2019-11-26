399bf58743aabafb1507c2d807799975ae7069ca
FanilAzm <fanil.azmagulov@mail.ru>
2019-11-26 14:11:45 +0500

    Добавил доп. задание по Fetch

---
+++ Cats and dogs/script.js
@@ -0,0 +1,33 @@
+const card = document.querySelector('.card'),
+    dogBtn = document.querySelector('.btn-dog'),
+    catBtn = document.querySelector('.btn-cat');
+
+dogBtn.addEventListener('click', () => {
+    fetch('https://random.dog/woof.json')
+        .then((response) => {
+            if(response.status !== 200){
+                throw new Error('Status network not 200');
+            }
+            return (response.json());
+        })
+        .then((response) => {
+            const url = response.url;
+            card.setAttribute('src', url);
+        })
+        .catch(error => console.error(error));
+});
+
+catBtn.addEventListener('click', () => {
+    fetch('https://aws.random.cat/meow')
+        .then((response) => {
+            if(response.status !== 200){
+                throw new Error('Status network not 200');
+            }
+            return (response.json());
+        })
+        .then((response) => {
+            const url = response.file;
+            card.setAttribute('src', url);
+        })
+        .catch(error => console.error(error));
+});
